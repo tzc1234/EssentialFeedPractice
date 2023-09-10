ce32a5bad317c4054f3633ebec943f96de2751d2
tzc1234 <peopli9997@gmail.com>
2023-09-10 10:55:12 +0100

    Add FeedImage model

---
+++ EssentialFeedPractice/FeedImage.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImage.swift
+//  EssentialFeedPractice
+//
+//  Created by Tsz-Lung on 10/09/2023.
+//
+
+import Foundation
+
+struct FeedImage {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
