5220583269523ce3b5530f98b5df38eef84ff739
tzc1234 <peopli9997@gmail.com>
2023-09-10 10:51:31 +0100

    Remove unnecessary code

--- EssentialFeedPractice/EssentialFeedPractice.h
+++ EssentialFeedPractice/EssentialFeedPractice.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double EssentialFeedPracticeVersionNumber;
 FOUNDATION_EXPORT const unsigned char EssentialFeedPracticeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedPractice/PublicHeader.h>
-
-
