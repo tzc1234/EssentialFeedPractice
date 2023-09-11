e22ee9730f794d8f92bd7647aaf72dca8c00d8ac
tzc1234 <peopli9997@gmail.com>
2023-09-11 10:00:49 +0100

    Save command requests deletion

--- EssentialFeedPractice/FeedImage.swift
+++ EssentialFeedPractice/FeedImage.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-struct FeedImage {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let imageURL: URL
+public struct FeedImage {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
--- EssentialFeedPracticeTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedPracticeTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -9,13 +9,27 @@ import XCTest
 import EssentialFeedPractice
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ feed: [FeedImage]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
-    private(set) var messages = [Any]()
+    private(set) var messages = [Message]()
+    
+    enum Message: Equatable {
+        case deletion
+    }
+    
+    func deleteCachedFeed() {
+        messages.append(.deletion)
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -24,5 +38,25 @@ final class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
         
         XCTAssertTrue(store.messages.isEmpty)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let feed = [uniqueItem()]
+        
+        sut.save(feed)
+        
+        XCTAssertEqual(store.messages, [.deletion])
+    }
+    
+    // MARK: - Helpers
+    
+    private func uniqueItem() -> FeedImage {
+        .init(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
     }
 }
