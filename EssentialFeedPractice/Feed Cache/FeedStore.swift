8209db196bc4df8fbd89c032bf01094a3b820ae0
tzc1234 <peopli9997@gmail.com>
2023-09-11 14:12:40 +0100

    Move LocalFeedImage to its own file

--- EssentialFeedPractice/Feed Cache/FeedStore.swift
+++ EssentialFeedPractice/Feed Cache/FeedStore.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-public struct LocalFeedImage: Equatable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let imageURL: URL
-    
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
-        self.id = id
-        self.description = description
-        self.location = location
-        self.imageURL = imageURL
-    }
-}
-
 public protocol FeedStore {
     func deleteCachedFeed(completion: @escaping (Result<Void, Error>) -> Void)
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping (Result<Void, Error>) -> Void)
