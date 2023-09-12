153e07a54d2c5fb8af85a864cd9e81564db655c8
tzc1234 <peopli9997@gmail.com>
2023-09-12 13:56:56 +0100

    Rename maxCacheAgeByDays to maxCacheAgeInDays

--- EssentialFeedPractice/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeedPractice/Feed Cache/FeedCachePolicy.swift
@@ -9,10 +9,10 @@ import Foundation
 
 enum FeedCachePolicy {
     private static let calendar = Calendar(identifier: .gregorian)
-    private static let maxCacheAgeByDays = 7
+    private static let maxCacheAgeInDays = 7
     
     static func validate(_ timestamp: Date, against date: Date) -> Bool {
-        guard let expirationDate = calendar.date(byAdding: .day, value: -maxCacheAgeByDays, to: date) else {
+        guard let expirationDate = calendar.date(byAdding: .day, value: -maxCacheAgeInDays, to: date) else {
             return false
         }
         
