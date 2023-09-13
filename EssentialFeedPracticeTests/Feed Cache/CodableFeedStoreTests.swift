38f0a372383ad15c094620a3084668859e396af2
tzc1234 <peopli9997@gmail.com>
2023-09-13 10:36:08 +0100

    Introduce makeSUT helper

--- EssentialFeedPracticeTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedPracticeTests/Feed Cache/CodableFeedStoreTests.swift
@@ -71,7 +71,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for cache retrieval")
         sut.retrieve { result in
@@ -87,7 +87,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for cache retrieval")
         sut.retrieve { firstResult in
@@ -105,7 +105,7 @@ final class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValue() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         let feed = uniqueFeed().locals
         let timestamp = Date()
         
@@ -127,5 +127,13 @@ final class CodableFeedStoreTests: XCTestCase {
             }
         }
         wait(for: [exp], timeout: 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
+        let sut = CodableFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
     }
 }
