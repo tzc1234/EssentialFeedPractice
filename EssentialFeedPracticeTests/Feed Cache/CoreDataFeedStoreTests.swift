2567f03d5bbb11020a8306141766e5ced1b8c678
tzc1234 <peopli9997@gmail.com>
2023-09-14 13:22:02 +0100

    Add makeSUT factory method helper

--- EssentialFeedPracticeTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedPracticeTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -24,13 +24,13 @@ class CoreDataFeedStore: FeedStore {
 
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CoreDataFeedStore()
+        let sut = makeSUT()
         
         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectOnEmptyCache() {
-        let sut = CoreDataFeedStore()
+        let sut = makeSUT()
         
         assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
@@ -73,5 +73,13 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_storeSideEffects_runSerially() {
         
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
     }
 }
