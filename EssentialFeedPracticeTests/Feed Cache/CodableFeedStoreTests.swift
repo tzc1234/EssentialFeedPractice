60c70875d13d8ad2ac30ce4a108c45fd5c56baf7
tzc1234 <peopli9997@gmail.com>
2023-09-15 10:34:22 +0100

    Extract the generic part of method swizzling into MethodSwizzlingStub super class

--- EssentialFeedPracticeTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedPracticeTests/Feed Cache/CodableFeedStoreTests.swift
@@ -174,31 +174,9 @@ private extension FileManager {
         .init(source: #selector(FileManager.removeItem(at:)), destination: #selector(Stub.removeItem))
     }
     
-    class Stub: NSObject {
-        private let source: Selector
-        private let destination: Selector
-
-        init(source: Selector, destination: Selector) {
-            self.source = source
-            self.destination = destination
-        }
-
+    class Stub: MethodSwizzlingStub<FileManager> {
         @objc func removeItem(at URL: URL) throws {
             throw anyNSError()
-        }
-
-        func startIntercepting() {
-            method_exchangeImplementations(
-                class_getInstanceMethod(FileManager.self, source)!,
-                class_getInstanceMethod(Stub.self, destination)!
-            )
-        }
-
-        deinit {
-            method_exchangeImplementations(
-                class_getInstanceMethod(Stub.self, destination)!,
-                class_getInstanceMethod(FileManager.self, source)!
-            )
         }
     }
 }
