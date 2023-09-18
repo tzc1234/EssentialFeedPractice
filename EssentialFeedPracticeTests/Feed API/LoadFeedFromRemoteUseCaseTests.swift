686bd82b28b99d456ad88c34b5c1d75fdb44e640
tzc1234 <peopli9997@gmail.com>
2023-09-18 14:31:23 +0100

    RemoteFeedLoader.load requests from URL

--- EssentialFeedPracticeTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedPracticeTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -9,30 +9,48 @@ import XCTest
 import EssentialFeedPractice
 
 class RemoteFeedLoader: FeedLoader {
-    init(client: HTTPClient) {
-        
+    private let client: HTTPClient
+    private let url: URL
+    
+    init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
     }
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        
+        client.get(from: url) { _ in }
     }
 }
 
 final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
     func test_init_doesNotRequestFromClientUponCreation() {
         let client = ClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(client: client, url: anyURL())
         
         XCTAssertEqual(client.requestCallCount, 0)
+    }
+    
+    func test_load_requestsFromURL() {
+        let client = ClientSpy()
+        let url = anyURL()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(client.loggedURLs, [url])
     }
     
     // MARK: - Helpers
     
     private class ClientSpy: HTTPClient {
-        private(set) var requestCallCount = 0
+        private(set) var loggedURLs = [URL]()
+        
+        var requestCallCount: Int {
+            loggedURLs.count
+        }
         
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-            
+            loggedURLs.append(url)
         }
     }
 }
