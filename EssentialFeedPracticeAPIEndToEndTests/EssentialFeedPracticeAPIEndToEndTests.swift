5032c147c7b9160651eba997032e59711a7b274c
tzc1234 <peopli9997@gmail.com>
2023-09-19 09:36:13 +0100

    Setup api end to end test target

---
+++ EssentialFeedPracticeAPIEndToEndTests/EssentialFeedPracticeAPIEndToEndTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedPracticeAPIEndToEndTests.swift
+//  EssentialFeedPracticeAPIEndToEndTests
+//
+//  Created by Tsz-Lung on 19/09/2023.
+//
+
+import XCTest
+
+final class EssentialFeedPracticeAPIEndToEndTests: XCTestCase {
+    
+}
