f9a99ae47aaaa8d9f2b715abc842980ef79dce37
tzc1234 <peopli9997@gmail.com>
2023-09-20 11:16:38 +0100

    Setup a clean environment for prototype

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  Prototype
+//
+//  Created by Tsz-Lung on 20/09/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ Prototype/Prototype/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  Prototype
+//
+//  Created by Tsz-Lung on 20/09/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
