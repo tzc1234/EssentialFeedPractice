81591f7fb90386cc42657bd9030fc8c337840b31
tzc1234 <peopli9997@gmail.com>
2023-09-21 13:46:37 +0100

    Load feed from viewDidLoad

--- EssentialFeedPracticeiOSTests/FeedViewControllerTests.swift
+++ EssentialFeedPracticeiOSTests/FeedViewControllerTests.swift
@@ -6,10 +6,22 @@
 //
 
 import XCTest
+import UIKit
 
-final class FeedViewController {
+final class FeedViewController: UITableViewController {
+    private let loader: FeedViewControllerTests.LoaderSpy
+    
     init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.loader = loader
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader.load()
     }
 }
 
@@ -21,9 +33,22 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     class LoaderSpy {
         private(set) var loadCallCount = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
