3b5923a704673676e6435e7589a5419d5843e096
tzc1234 <peopli9997@gmail.com>
2023-09-21 13:39:29 +0100

    Does not load feed upon FeedViewController cretaion

---
+++ EssentialFeedPracticeiOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeedPracticeiOSTests
+//
+//  Created by Tsz-Lung on 21/09/2023.
+//
+
+import XCTest
+
+final class FeedViewController {
+    init(loader: FeedViewControllerTests.LoaderSpy) {
+        
+    }
+}
+
+final class FeedViewControllerTests: XCTestCase {
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        _ = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    // MARK: - Helpers
+    
+    class LoaderSpy {
+        private(set) var loadCallCount = 0
+    }
+}
