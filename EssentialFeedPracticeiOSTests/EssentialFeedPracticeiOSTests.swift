49709004267917e1c526ce184440d2904e3deb6f
tzc1234 <peopli9997@gmail.com>
2023-09-21 11:43:52 +0100

    Setup for EssentialFeedPracticeiOS target

---
+++ EssentialFeedPracticeiOSTests/EssentialFeedPracticeiOSTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedPracticeiOSTests.swift
+//  EssentialFeedPracticeiOSTests
+//
+//  Created by Tsz-Lung on 21/09/2023.
+//
+
+import XCTest
+
+final class EssentialFeedPracticeiOSTests: XCTestCase {
+    func test_() {
+        
+    }
+}
