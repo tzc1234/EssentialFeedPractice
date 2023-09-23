fd66b303d7c4467f0fa8ccecbde570ee1d06e20f
tzc1234 <peopli9997@gmail.com>
2023-09-23 17:13:31 +0100

    Move UIRefreshControl test helpers to its own file

--- EssentialFeedPracticeiOSTests/FeedViewControllerTests.swift
+++ EssentialFeedPracticeiOSTests/FeedViewControllerTests.swift
@@ -454,38 +454,3 @@ extension FeedImageCell {
         feedImageRetryButton.simulate(event: .touchUpInside)
     }
 }
-
-private extension UIRefreshControl {
-    static func refreshingStub() -> Stub {
-        Stub(methodPairs: [
-            .init(
-                source: #selector(getter: Stub.isRefreshing),
-                destination: #selector(getter: UIRefreshControl.isRefreshing)
-            ),
-            .init(
-                source: #selector(Stub.beginRefreshing),
-                destination: #selector(UIRefreshControl.beginRefreshing)
-            ),
-            .init(
-                source: #selector(Stub.endRefreshing),
-                destination: #selector(UIRefreshControl.endRefreshing)
-            )
-        ])
-    }
-    
-    class Stub: MethodSwizzlingStub<UIRefreshControl> {
-        @objc private(set) var isRefreshing = false
-        
-        @objc func beginRefreshing() {
-            isRefreshing = true
-        }
-        
-        @objc func endRefreshing() {
-            isRefreshing = false
-        }
-    }
-    
-    func simulatePullToRefresh() {
-        simulate(event: .valueChanged)
-    }
-}
