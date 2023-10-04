fc25fce0ed073094b668b85fbf68956a67d639b3
tzc1234 <peopli9997@gmail.com>
2023-10-04 15:02:42 +0100

    Move FeedImagePresenter and collaborators to production

---
+++ EssentialFeedPractice/Feed Presentation/FeedImagePresenter.swift
@@ -0,0 +1,69 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeedPractice
+//
+//  Created by Tsz-Lung on 04/10/2023.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let description: String?
+    public let location: String?
+    public let image: Image?
+    public let shouldRetry: Bool
+}
+
+public struct FeedImageLoadingViewModel {
+    public let isLoading: Bool
+}
+
+public protocol FeedImageView {
+    associatedtype Image
+    
+    func display(_ viewModel: FeedImageViewModel<Image>)
+}
+
+public protocol FeedImageLoadingView {
+    func display(_ viewModel: FeedImageLoadingViewModel)
+}
+
+public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private let loadingView: FeedImageLoadingView
+    private let imageTransformer: (Data) -> Image?
+    
+    public init(view: View, loadingView: FeedImageLoadingView, imageTransformer: @escaping (Data) -> Image?) {
+        self.view = view
+        self.loadingView = loadingView
+        self.imageTransformer = imageTransformer
+    }
+    
+    public func didStartLoadingImageData(for model: FeedImage) {
+        loadingView.display(FeedImageLoadingViewModel(isLoading: true))
+        view.display(FeedImageViewModel<Image>(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            shouldRetry: false))
+    }
+    
+    public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        let image = imageTransformer(data)
+        view.display(FeedImageViewModel<Image>(
+            description: model.description,
+            location: model.location,
+            image: image,
+            shouldRetry: image == nil))
+        loadingView.display(FeedImageLoadingViewModel(isLoading: false))
+    }
+    
+    public func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+        view.display(FeedImageViewModel<Image>(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            shouldRetry: true))
+        loadingView.display(FeedImageLoadingViewModel(isLoading: false))
+    }
+}
--- EssentialFeedPracticeTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedPracticeTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -8,67 +8,6 @@
 import XCTest
 import EssentialFeedPractice
 
-struct FeedImageViewModel<Image> {
-    let description: String?
-    let location: String?
-    let image: Image?
-    let shouldRetry: Bool
-}
-
-struct FeedImageLoadingViewModel {
-    let isLoading: Bool
-}
-
-protocol FeedImageView {
-    associatedtype Image
-    
-    func display(_ viewModel: FeedImageViewModel<Image>)
-}
-
-protocol FeedImageLoadingView {
-    func display(_ viewModel: FeedImageLoadingViewModel)
-}
-
-final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
-    private let view: View
-    private let loadingView: FeedImageLoadingView
-    private let imageTransformer: (Data) -> Image?
-    
-    init(view: View, loadingView: FeedImageLoadingView, imageTransformer: @escaping (Data) -> Image?) {
-        self.view = view
-        self.loadingView = loadingView
-        self.imageTransformer = imageTransformer
-    }
-    
-    func didStartLoadingImageData(for model: FeedImage) {
-        loadingView.display(FeedImageLoadingViewModel(isLoading: true))
-        view.display(FeedImageViewModel<Image>(
-            description: model.description,
-            location: model.location,
-            image: nil,
-            shouldRetry: false))
-    }
-    
-    func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
-        let image = imageTransformer(data)
-        view.display(FeedImageViewModel<Image>(
-            description: model.description,
-            location: model.location,
-            image: image,
-            shouldRetry: image == nil))
-        loadingView.display(FeedImageLoadingViewModel(isLoading: false))
-    }
-    
-    func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
-        view.display(FeedImageViewModel<Image>(
-            description: model.description,
-            location: model.location,
-            image: nil,
-            shouldRetry: true))
-        loadingView.display(FeedImageLoadingViewModel(isLoading: false))
-    }
-}
-
 final class FeedImagePresenterTests: XCTestCase {
     func test_init_doesNotMessageView() {
         let (_, view) = makeSUT()
