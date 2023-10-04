7c2b1de4f9c72cb74003a8bd7ec0926c21386db4
tzc1234 <peopli9997@gmail.com>
2023-10-04 11:31:38 +0100

    Move FeedErrorViewModel to its own file

--- EssentialFeedPractice/Feed Presentation/FeedPresenter.swift
+++ EssentialFeedPractice/Feed Presentation/FeedPresenter.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-public struct FeedErrorViewModel {
-    public let message: String?
-    
-    static var noError: FeedErrorViewModel {
-        FeedErrorViewModel(message: nil)
-    }
-    
-    static func error(message: String) -> FeedErrorViewModel {
-        FeedErrorViewModel(message: message)
-    }
-}
-
 public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
