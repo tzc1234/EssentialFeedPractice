4e48eecce44af68b62332c9ede85f725c324856a
tzc1234 <peopli9997@gmail.com>
2023-10-06 15:40:57 +0100

    Introduce makeSUT helpers for LoadFeedImageDataFromRemoteUseCaseTests

--- EssentialFeedPracticeTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ EssentialFeedPracticeTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -24,15 +24,13 @@ final class RemoteFeedImageDataLoader {
 
 final class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     func test_init_doesNotRequestFromClientUponCreation() {
-        let client = HTTPClientSpy()
-        _ = RemoteFeedImageDataLoader(client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
-    func test_loadImageData_requestsDataFromURL() {
-        let client = HTTPClientSpy()
-        let sut = RemoteFeedImageDataLoader(client: client)
+    func test_loadImageDataFromURL_requestsDataFromURL() {
+        let (sut, client) = makeSUT()
         let url = URL(string: "https://a-given-url.com")!
         
         sut.loadImageData(for: url)
@@ -41,6 +39,15 @@ final class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, client)
+    }
     
     private class HTTPClientSpy: HTTPClient {
         private(set) var requestedURLs = [URL]()
