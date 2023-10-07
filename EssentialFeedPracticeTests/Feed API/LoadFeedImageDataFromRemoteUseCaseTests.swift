d7c6ddd8797e408338df8996c1e3c1648d83f0a3
tzc1234 <peopli9997@gmail.com>
2023-10-07 10:48:02 +0100

    Move RemoteFeedImageDataLoader to production

--- EssentialFeedPracticeTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
+++ EssentialFeedPracticeTests/Feed API/LoadFeedImageDataFromRemoteUseCaseTests.swift
@@ -8,62 +8,6 @@
 import XCTest
 import EssentialFeedPractice
 
-final class RemoteFeedImageDataLoader: FeedImageDataLoader {
-    private let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
-        var wrapped: HTTPClientTask?
-        
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-        
-        func cancel() {
-            preventFurtherCompletions()
-            wrapped?.cancel()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
-        }
-    }
-    
-    func loadImageData(from url: URL,
-                       completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let taskWrapper = HTTPClientTaskWrapper(completion)
-        taskWrapper.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            taskWrapper.complete(with: result
-                .mapError { _ in Error.connectivity }
-                .flatMap { (data, response) in
-                    let isValidResponse = Self.isOK(response) && !data.isEmpty
-                    return isValidResponse ? .success(data) : .failure(Error.invalidData)
-                })
-        }
-        return taskWrapper
-    }
-    
-    private static func isOK(_ response: HTTPURLResponse) -> Bool {
-        response.statusCode == 200
-    }
-}
-
 final class LoadFeedImageDataFromRemoteUseCaseTests: XCTestCase {
     func test_init_doesNotRequestFromClientUponCreation() {
         let (_, client) = makeSUT()
