fd9e84e98aabae5fb22a32112db9295c6cf69995
tzc1234 <peopli9997@gmail.com>
2023-10-08 15:29:21 +0100

    LoadFeedImageDataLoader does not message store upon creation (in cache feed image data context)

---
+++ EssentialFeedPracticeTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  CacheFeedImageDataUseCaseTests.swift
+//  EssentialFeedPracticeTests
+//
+//  Created by Tsz-Lung on 08/10/2023.
+//
+
+import XCTest
+import EssentialFeedPractice
+
+final class CacheFeedImageDataUseCaseTests: XCTestCase {
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertTrue(store.messages.isEmpty)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
+        let store = FeedImageDataStoreSpy()
+        let sut = LocalFeedImageDataLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+}
