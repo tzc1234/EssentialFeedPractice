ba97b65adc2b081d56143cf90426bf9462d1c9db
tzc1234 <peopli9997@gmail.com>
2023-10-09 13:04:17 +0100

    Guarantee CoreDataFeedStore side effects run serially

--- EssentialFeedPracticeTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedPracticeTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -47,6 +47,22 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toRetrieveWith: .success(lastStoreData), for: url)
     }
     
+    func test_sideEffects_runSerially() {
+        let sut = makeSUT()
+        let url = anyURL()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert([localImage(url: url)], timestamp: Date()) { _ in op1.fulfill() }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.insert(anyData(), for: url) { _ in op2.fulfill() }
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(anyData(), for: url) { _ in op3.fulfill() }
+        
+        wait(for: [op1, op2, op3], timeout: 5, enforceOrder: true)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
@@ -81,17 +97,15 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
             switch result {
             case .success:
                 sut.insert(data, for: url) { result in
-                    switch result {
-                    case .success:
-                        break
-                    case let .failure(error):
+                    if case let .failure(error) = result {
                         XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
                     }
+                    exp.fulfill()
                 }
             case let .failure(error):
                 XCTFail("Failed to save \(image) with error \(error)", file: file, line: line)
+                exp.fulfill()
             }
-            exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
     }
