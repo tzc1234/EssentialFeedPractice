9ecd229756937a1274fcc452befa406292641490
tzc1234 <peopli9997@gmail.com>
2023-10-09 13:12:33 +0100

    Move CoreDataFeedStore implementation of the FeedStore protocol to separate file

--- EssentialFeedPractice/Feed Cache/Infrastucture/CoreData/CoreDataFeedStore.swift
+++ EssentialFeedPractice/Feed Cache/Infrastucture/CoreData/CoreDataFeedStore.swift
@@ -7,56 +7,13 @@
 
 import CoreData
 
-public class CoreDataFeedStore: FeedStore {
+public class CoreDataFeedStore {
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
     public init(storeURL: URL) throws {
         self.container = try Self.loadContainer(for: storeURL)
         self.context = container.newBackgroundContext()
-    }
-    
-    public func retrieve(completion: @escaping RetrieveCompletion) {
-        perform { context in
-            do {
-                guard let managedCache = try ManagedCache.find(by: context) else {
-                    completion(.success(.none))
-                    return
-                }
-                
-                completion(.success((managedCache.localFeed, managedCache.timestamp)))
-            } catch {
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertCompletion) {
-        perform { context in
-            do {
-                let managedCache = try ManagedCache.newUniqueInstance(in: context)
-                managedCache.timestamp = timestamp
-                managedCache.feed = feed.toManagedFeed(in: context)
-                
-                try context.save()
-                completion(.success(()))
-            } catch {
-                context.rollback()
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    public func deleteCachedFeed(completion: @escaping DeleteCompletion) {
-        perform { context in
-            do {
-                try ManagedCache.find(by: context).map(context.delete).map(context.save)
-                completion(.success(()))
-            } catch {
-                context.rollback()
-                completion(.failure(error))
-            }
-        }
     }
     
     func perform(_ block: @escaping (NSManagedObjectContext) -> Void) {
@@ -97,18 +54,5 @@ extension CoreDataFeedStore {
         }
         
         return model
-    }
-}
-
-private extension [LocalFeedImage] {
-    func toManagedFeed(in context: NSManagedObjectContext) -> NSOrderedSet {
-        NSOrderedSet(array: map { local in
-            let managed = ManagedFeedImage(context: context)
-            managed.id = local.id
-            managed.imageDescription = local.description
-            managed.location = local.location
-            managed.url = local.url
-            return managed
-        })
     }
 }
