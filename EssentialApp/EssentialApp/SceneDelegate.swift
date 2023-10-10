92bce074c034fdb21aaf04770d05c9dc0bd30511
tzc1234 <peopli9997@gmail.com>
2023-10-10 10:13:46 +0100

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  EssentialApp
+//
+//  Created by Tsz-Lung on 10/10/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
