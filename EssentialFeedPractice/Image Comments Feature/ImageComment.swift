dbf4da4b045f5e75ad433fdd683ac333e5bdfa86
tzc1234 <peopli9997@gmail.com>
2023-10-23 16:16:56 +0100

    Add ImageComment model

---
+++ EssentialFeedPractice/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComment.swift
+//  EssentialFeedPractice
+//
+//  Created by Tsz-Lung on 23/10/2023.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
