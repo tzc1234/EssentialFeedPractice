d07772b9e45a2f9fe153f35972f1ccb854dd3480
tzc1234 <peopli9997@gmail.com>
2023-10-28 13:00:00 +0100

    Move CellController to its own file

--- EssentialFeedPracticeiOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeedPracticeiOS/Shared UI/Controllers/ListViewController.swift
@@ -8,24 +8,6 @@
 import UIKit
 import EssentialFeedPractice
 
-public struct CellController {
-    let dataSource: UITableViewDataSource
-    let delegate: UITableViewDelegate?
-    let dataSourcePrefetching: UITableViewDataSourcePrefetching?
-    
-    public init(_ dataSource: UITableViewDataSource & UITableViewDelegate & UITableViewDataSourcePrefetching) {
-        self.dataSource = dataSource
-        self.delegate = dataSource
-        self.dataSourcePrefetching = dataSource
-    }
-    
-    public init(_ dataSource: UITableViewDataSource) {
-        self.dataSource = dataSource
-        self.delegate = nil
-        self.dataSourcePrefetching = nil
-    }
-}
-
 public final class ListViewController: UITableViewController {
     public let errorView = ErrorView()
     
