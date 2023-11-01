3a639b76025df2fd7e5677597570709b7a7f9c30
tzc1234 <peopli9997@gmail.com>
2023-11-01 13:42:45 +0000

    Cache page results

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -94,7 +94,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                                           last: FeedImage?) -> (() -> AnyPublisher<Paginated<FeedImage>, Error>)? {
         last.map { lastItem in
             let url = FeedEndpoint.get(after: lastItem).url(baseURL: baseURL)
-            return { [httpClient] in
+            return { [httpClient, localFeedLoader] in
                 httpClient
                     .getPublisher(url: url)
                     .tryMap(FeedItemsMapper.map)
@@ -104,7 +104,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                             items: allItems,
                             loadMorePublisher: self.makeRemoteLoadMoreLoader(items: allItems, last: newItems.last))
                     }
-                    .eraseToAnyPublisher()
+                    .caching(to: localFeedLoader)
             }
         }
     }
