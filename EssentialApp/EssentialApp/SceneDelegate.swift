4a7ccb24e62409c556f3bbd0036273ce84e9f6c8
tzc1234 <peopli9997@gmail.com>
2023-11-29 09:54:50 +0000

    Group Combine and publisher helpers

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -143,36 +143,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .eraseToAnyPublisher()
     }
 }
-
-extension Publisher {
-    func logCacheMisses(url: URL, logger: Logger) -> AnyPublisher<Output, Failure> {
-        handleEvents(receiveCompletion: { result in
-            if case .failure = result {
-                logger.trace("Cache miss for url: \(url)")
-            }
-        })
-        .eraseToAnyPublisher()
-    }
-    
-    func logError(url: URL, logger: Logger) ->  AnyPublisher<Output, Failure> {
-        handleEvents(receiveCompletion: { result in
-            if case let .failure(error) = result {
-                logger.trace("Failed to load url: \(url) with error: \(error)")
-            }
-        })
-        .eraseToAnyPublisher()
-    }
-    
-    func logElapsedTime(url: URL, logger: Logger) -> AnyPublisher<Output, Failure> {
-        var startTime = CACurrentMediaTime()
-        
-        return handleEvents(receiveSubscription: { _ in
-            logger.trace("Started loading url: \(url)")
-            startTime = CACurrentMediaTime()
-        }, receiveCompletion: { _ in
-            let elapsed = CACurrentMediaTime() - startTime
-            logger.trace("Finished loading url: \(url) in \(elapsed) seconds")
-        })
-        .eraseToAnyPublisher()
-    }
-}
